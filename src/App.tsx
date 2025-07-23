f6ae8909d4bdbd8e8eebd03523aa7c74b018ac17
Diego Luiz De Luca <dlluca@mppr.mp.br>
2025-07-23 14:40:15 -0300

    Semana 3-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
 import './App.css';
+import { CartProvider } from './context/CartContext';
+import { SearchProvider } from './context/SearchContext';
 import AppRouter from './routes';
-import { SearchContext } from './context/SearchContext';
 
 function App() {
-  const [search, setSearch] = useState('');
-
   return (
-    <SearchContext value={{search: search, setSearch: setSearch}}>
-      <AppRouter />
-    </SearchContext>
+    <CartProvider>
+      <SearchProvider>
+        <AppRouter />
+      </SearchProvider>
+    </CartProvider>
   );
 }
 
